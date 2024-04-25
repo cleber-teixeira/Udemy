965182aa4575faedb212d6a9e6923ac5a45198de
DESKTOP-JV7IJVI\Cleber <cleberlt@gmail.com>
2024-04-25 19:35:54 -0300

    Criação do pacote dos exercícios de estrutura_sequencial.

---
+++ Java_Completo/estrutura_sequencial/src/exercicios/exercicio_01.java
@@ -0,0 +1,13 @@
+package exercicios;
+
+public class exercicio_01 {
+	/* TODO Faça um programa para ler dois valores inteiros,
+	 * e depois mostrar na tela a soma desses números com uma
+	 * mensagem explicativa, conforme exemplos. */
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
