bf492ac622cffb202b348e23bfae63360b26ddae
DESKTOP-JV7IJVI\Cleber <cleberlt@gmail.com>
2024-04-25 19:49:50 -0300

    exercício 01

--- Java_Completo/estrutura_sequencial/src/exercicios/exercicio_01.java
+++ Java_Completo/estrutura_sequencial/src/exercicios/exercicio_01.java
@@ -1,5 +1,7 @@
 package exercicios;
 
+import java.util.Scanner;
+
 public class exercicio_01 {
 	/* TODO Faça um programa para ler dois valores inteiros,
 	 * e depois mostrar na tela a soma desses números com uma
@@ -7,7 +9,18 @@ public class exercicio_01 {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		
+		int value01, value02, result;
+		
+		System.out.println("Soma de dois valores inteiros.\n");
+		System.out.println("Digite o primeiro valor:");
+		value01 = sc.nextInt();
+		System.out.println("\nDigite o segundo valor:");
+		value02 = sc.nextInt();
+		
+		result = value01 + value02;
+		System.out.println("\nSOMA = " + result);
 	}
 
 }
