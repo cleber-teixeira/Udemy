cc69cd1f5bf8bded5a358871e091ef9032a3f50f
Cleber <cleberlt@gmail.com>
2024-04-26 14:12:45 -0300

    update - exercício 03

--- Java_Completo/estrutura_sequencial/src/exercicios/exercicio_03.java
+++ Java_Completo/estrutura_sequencial/src/exercicios/exercicio_03.java
@@ -2,7 +2,7 @@ package exercicios;
 
 import java.util.Scanner;
 
-public class exerciocio_03 {
+public class exercicio_03 {
 	/* TODO Fazer um programa para ler quatro valores inteiros A, B, C e D.
 	 * A seguir, calcule e mostre a diferença do produto de A e B pelo produto
 	 * de C e D segundo a fórmula: DIFERENCA = (A * B - C * D). */
