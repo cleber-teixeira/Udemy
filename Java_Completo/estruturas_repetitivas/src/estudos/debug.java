4a9bc5f62ad0a4490b784d99abb377d81c87cd5b
DESKTOP-JV7IJVI\Cleber <cleberlt@gmail.com>
2024-05-02 16:40:16 -0300

    update

--- Java_Completo/estruturas_repetitivas/src/estudos/debug.java
+++ Java_Completo/estruturas_repetitivas/src/estudos/debug.java
@@ -4,7 +4,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class debug {
-	// TODO 
+	// TODO Programa que calcula o preço do terreno.
 
 	public static void main(String[] args) {
 
