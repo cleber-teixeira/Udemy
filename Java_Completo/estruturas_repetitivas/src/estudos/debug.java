a56468275c616495251d619a0c8ef9cd96dd2162
DESKTOP-JV7IJVI\Cleber <cleberlt@gmail.com>
2024-05-02 16:14:53 -0300

    aula 43 - (Como utilizar o DEBUG no Eclipse).

---
+++ Java_Completo/estruturas_repetitivas/src/estudos/debug.java
@@ -0,0 +1,32 @@
+package estudos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class debug {
+	// TODO 
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite a largura do terreno: ");
+		double largura = sc.nextDouble();
+		
+		System.out.print("Digite o comprimento do terreno: ");
+		double comprimento = sc.nextDouble();
+		
+		System.out.print("Digite o valor do m²: ");
+		double metroQuadrado = sc.nextDouble();
+		
+		double area = largura * comprimento;
+		double preco = area * metroQuadrado;
+		
+		System.out.printf("\nAREA = %.2f%n", area);
+		System.out.printf("PRECO = R$ %.2f%n", preco);
+		
+		sc.close();
+		
+	}
+}
