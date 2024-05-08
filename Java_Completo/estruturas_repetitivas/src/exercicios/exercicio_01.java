4aabe80b7799e12132f01be655263bcd71e0725e
DESKTOP-JV7IJVI\Cleber <cleberlt@gmail.com>
2024-05-08 16:28:32 -0300

    :books: docs: atualiza Javadoc.

--- Java_Completo/estruturas_repetitivas/src/exercicios/exercicio_01.java
+++ Java_Completo/estruturas_repetitivas/src/exercicios/exercicio_01.java
@@ -2,22 +2,22 @@ package exercicios;
 
 import java.util.Scanner;
 
+/** Programa que lê a senha e retorna 'Senha Invalida' ou 'Acesso Permitido'.
+ * 
+ * @author Cleber Teixeira
+ * @category Exercícios
+ * @section 6 - Estruturas repetitivas
+ * @lesson 48 - Exercícios para iniciantes - Parte 3)
+ */
 public class exercicio_01 {
-	/** Programa que lê números inteiros e retorna a soma.
-	 * 
-	 * @author Cleber Teixeira
-	 * @category Exercícios
-	 * @section 6 - Estruturas repetitivas
-	 * @lesson 48 - Exercícios para iniciantes - Parte 3)
-	 */
+	/* TODO Escreva um programa que repita a leitura de uma senha
+	 * até que ela seja válida. Para cada leitura de senha incorreta
+	 * informada, escrever a mensagem "Senha Invalida". Quando a
+	 * senha for informada corretamente deve ser impressa a mensagem
+	 * "Acesso Permitido" e o algoritmo encerrado. Considere que a
+	 * senha correta é o valor 2002. */
 
 	public static void main(String[] args) {
-		/* TODO Escreva um programa que repita a leitura de uma senha
-		 * até que ela seja válida. Para cada leitura de senha incorreta
-		 * informada, escrever a mensagem "Senha Invalida". Quando a
-		 * senha for informada corretamente deve ser impressa a mensagem
-		 * "Acesso Permitido" e o algoritmo encerrado. Considere que a
-		 * senha correta é o valor 2002. */
 		Scanner sc = new Scanner(System.in);
 		
 		int password = 0;
